5bcef3db9aa52ad44093880ac53e9cff6600c4e4
KarimElsayad247 <karimelsayad1999@gmail.com>
2022-10-30 17:29:00 +0200

    Display books in a table

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,55 @@
-console.log("hello world")
+let myLibrary = []
+
+function Book(title, author, pages) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = false;
+}
+
+Book.prototype.info = function() {
+    return `${this.title} by ${this.author}, ${this.pages} pages, ` +
+           `${this.isRead ? "Has been read" : "not read yet"}`;
+}
+
+function addBookToLibrary(book) {
+    myLibrary.push(book)
+}
+
+function displayBooksInTable() {
+    let booksTable = document.querySelector(".books-container");
+    for (const book of myLibrary) {
+        let bookRow = createRow(book);
+        booksTable.appendChild(bookRow);
+    }
+}
+
+function createRow(book) {
+    let row = document.createElement('tr');
+
+    let title = document.createElement('td');
+    title.innerText = book.title;
+
+    let author = document.createElement('td');
+    author.innerText = book.author;
+
+    let pages = document.createElement('td');
+    pages.innerText = book.pages;
+
+    let isRead = document.createElement('td');
+    isRead.innerText = book.isRead ? "Read" : "Not read";
+
+    row.appendChild(title);
+    row.appendChild(author);
+    row.appendChild(pages);
+    row.appendChild(isRead);
+
+    return row;
+}
+
+let theHobbit = new Book("The Hobbit", "JRR Tolkien", 295)
+
+addBookToLibrary(theHobbit)
+displayBooksInTable()
+
+console.log(theHobbit.info());
